1ab82d95d8244f013566c7d326480455e53c41cb
Georg Lang <georglang0788@gmx.net>
2017-03-14 20:34:36 +0100

    implemented CRUD operations + mongoose schema

--- server.js
+++ server.js
@@ -1,42 +1,95 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-app.set('view engine', 'ejs');
+const Router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const DiaryEntries = require('./models/diary.js');
+const port = 3000;
 
-var db;
 
-MongoClient.connect('connection-data', (err, database) => {
-  if (err)
-  {
-    return console.log(err)
-  }
-  db = database
 
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
+// get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.use(function (req, res, next) {
+  console.log('Something is happening');
+  next();
 });
 
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// Router.route() to handle multiple routes for the same URI
+Router.route('/diary')
+  .post(function (req, res) {
+    var diaryEntry = new DiaryEntries();
+    diaryEntry.name = req.body.name;
 
-app.get('/', (req,res) => {
-  db.collection('quotes').find().toArray(function(err, result) {
-    res.render('index.ejs', {quotes: result});
+    diaryEntry.save(function (err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({message: "diaryEntry created"})
+    })
+  })
+
+  .get(function (req, res) {
+    DiaryEntries.find(function (err, entires) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(entires);
+    })
   });
-});
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err)
-    {
-      return console.log(err)
-    }
 
-    console.log('saved to database');
-    res.redirect('/');
+Router.route('/diary/:entry_id')
+  .get(function (req, res) {
+    DiaryEntries.findById(req.params.entry_id, function (err, entry) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(entry);
+    })
   })
-});
+
+  .put(function(req, res) {
+    DiaryEntries.findById(req.params.entry_id, function(err, entry) {
+
+      if (err)
+        res.send(err);
+
+      console.log('Request.Body.Name', req.body.name);
+      entry.name = req.body.name;  // update the name
+
+      // save the entry
+      entry.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'Entry updated!' });
+      });
+
+    });
+  })
+
+  .delete(function (req, res) {
+    DiaryEntries.remove({
+      _id: req.params.entry_id
+    }, function (err, entry) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({message: 'Successfully deleted'});
+    })
+  });
+
+
+//register routes
+// all of the routes will be prefixed with /api
+app.use('/api', Router);
+
+//start server
+app.listen(port);
+
+//
