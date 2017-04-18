3d8b4b1c8a714f8887c51e098854e337716b91b1
Georg Lang <georglang0788@gmx.net>
2017-04-18 22:59:55 +0200

    message model

---
+++ server/models/message.model.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Message = mongoose.model('Message', messageSchema)
+
+export default Message;
